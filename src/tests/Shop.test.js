ad3b8662824331272bde7f6587629bb186436fe2
Nick Calibey <nick.calibey@gmail.com>
2018-10-10 15:11:02 -0500

    added some tests for the Shop component

--- src/tests/Shop.test.js
+++ src/tests/Shop.test.js
@@ -2,8 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import Shop from '../components/Shop';
+import ProductsList from '../components/ProductsList';
 
-it('has an h1 element', () => {
-  const wrapper = shallow(<Shop />);
-  expect(wrapper.find('h1').text()).toEqual('Welcome!');
+describe ('Shop', () => {
+  it('has an h1 element', () => {
+    const wrapper = shallow(<Shop />);
+
+    expect(wrapper.find('h1').text()).toEqual('The Shop!');
+  });
+
+  it ('renders one <ProductsList /> component', () => {
+    const wrapper = shallow(<Shop />);
+
+    expect(wrapper.find(ProductsList).length).toBe(1);
+  });
+
+  it ('updates cart state when an item is added', () => {
+    const wrapper = shallow(<Shop />);
+
+    expect(wrapper.state().cart.length).toBe(0);
+    wrapper.instance().updateCart('1');
+    expect(wrapper.state().cart.length).toBe(1);
+  });
 });
